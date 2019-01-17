684f55a1943b9128cf1afecf1addbdee46e79077
GCL <lewin.gc@gmail.com>
2019-01-17 11:35:38 -0500

    streamlined

--- BME280.cpp
+++ BME280.cpp
@@ -21,159 +21,141 @@ Distributed as-is; no warranty is given.
 //See SparkFunBME280.h for additional topology notes.
 
 #include "BME280.h"
-#include "stdint.h"
 #include <math.h>
-
 #include "Wire.h"
-#include "SPI.h"
+
+//Register names:
+#define BME280_DIG_T1_LSB_REG            0x88
+#define BME280_DIG_T1_MSB_REG            0x89
+#define BME280_DIG_T2_LSB_REG            0x8A
+#define BME280_DIG_T2_MSB_REG            0x8B
+#define BME280_DIG_T3_LSB_REG            0x8C
+#define BME280_DIG_T3_MSB_REG            0x8D
+#define BME280_DIG_P1_LSB_REG            0x8E
+#define BME280_DIG_P1_MSB_REG            0x8F
+#define BME280_DIG_P2_LSB_REG            0x90
+#define BME280_DIG_P2_MSB_REG            0x91
+#define BME280_DIG_P3_LSB_REG            0x92
+#define BME280_DIG_P3_MSB_REG            0x93
+#define BME280_DIG_P4_LSB_REG            0x94
+#define BME280_DIG_P4_MSB_REG            0x95
+#define BME280_DIG_P5_LSB_REG            0x96
+#define BME280_DIG_P5_MSB_REG            0x97
+#define BME280_DIG_P6_LSB_REG            0x98
+#define BME280_DIG_P6_MSB_REG            0x99
+#define BME280_DIG_P7_LSB_REG            0x9A
+#define BME280_DIG_P7_MSB_REG            0x9B
+#define BME280_DIG_P8_LSB_REG            0x9C
+#define BME280_DIG_P8_MSB_REG            0x9D
+#define BME280_DIG_P9_LSB_REG            0x9E
+#define BME280_DIG_P9_MSB_REG            0x9F
+#define BME280_DIG_H1_REG                0xA1
+#define BME280_CHIP_ID_REG               0xD0 //Chip ID
+#define BME280_RST_REG                   0xE0 //Softreset Reg
+#define BME280_DIG_H2_LSB_REG            0xE1
+#define BME280_DIG_H2_MSB_REG            0xE2
+#define BME280_DIG_H3_REG                0xE3
+#define BME280_DIG_H4_MSB_REG            0xE4
+#define BME280_DIG_H4_LSB_REG            0xE5
+#define BME280_DIG_H5_MSB_REG            0xE6
+#define BME280_DIG_H6_REG                0xE7
+#define BME280_CTRL_HUMIDITY_REG        0xF2 //Ctrl Humidity Reg
+#define BME280_STAT_REG                 0xF3 //Status Reg
+#define BME280_CTRL_MEAS_REG            0xF4 //Ctrl Measure Reg
+#define BME280_CONFIG_REG               0xF5 //Configuration Reg
+#define BME280_PRESSURE_MSB_REG         0xF7 //Pressure MSB
+#define BME280_PRESSURE_LSB_REG         0xF8 //Pressure LSB
+#define BME280_PRESSURE_XLSB_REG        0xF9 //Pressure XLSB
+#define BME280_TEMPERATURE_MSB_REG      0xFA //Temperature MSB
+#define BME280_TEMPERATURE_LSB_REG      0xFB //Temperature LSB
+#define BME280_TEMPERATURE_XLSB_REG     0xFC //Temperature XLSB
+#define BME280_HUMIDITY_MSB_REG         0xFD //Humidity MSB
+#define BME280_HUMIDITY_LSB_REG         0xFE //Humidity LSB
 
 //****************************************************************************//
 //
 //  Configuration section
 //
-//  This uses the stored SensorSettings to start the IMU
-//  Use statements such as "mySensor.settings.commInterface = SPI_MODE;" to 
-//  configure before calling .begin();
-//
 //****************************************************************************//
-uint8_t BME280::Init(const BME280Settings& s)
+uint8_t BME280::Init(void)
 {
-    settings = s;
-    
-	//Check the settings structure values to determine how to setup the device
-	uint8_t dataToWrite = 0;  //Temporary variable
-
     Wire.begin();
 	
 	//Reading all compensation data, range 0x88:A1, 0xE1:E7
 	
-	calibration.dig_T1 = ((uint16_t)((readRegister(BME280_DIG_T1_MSB_REG) << 8) + readRegister(BME280_DIG_T1_LSB_REG)));
-	calibration.dig_T2 = ((int16_t)((readRegister(BME280_DIG_T2_MSB_REG) << 8) + readRegister(BME280_DIG_T2_LSB_REG)));
-	calibration.dig_T3 = ((int16_t)((readRegister(BME280_DIG_T3_MSB_REG) << 8) + readRegister(BME280_DIG_T3_LSB_REG)));
+	calibrationData.dig_T1 = ((uint16_t)((readRegister(BME280_DIG_T1_MSB_REG) << 8) + readRegister(BME280_DIG_T1_LSB_REG)));
+	calibrationData.dig_T2 = ((int16_t)((readRegister(BME280_DIG_T2_MSB_REG) << 8) + readRegister(BME280_DIG_T2_LSB_REG)));
+	calibrationData.dig_T3 = ((int16_t)((readRegister(BME280_DIG_T3_MSB_REG) << 8) + readRegister(BME280_DIG_T3_LSB_REG)));
 
-	calibration.dig_P1 = ((uint16_t)((readRegister(BME280_DIG_P1_MSB_REG) << 8) + readRegister(BME280_DIG_P1_LSB_REG)));
-	calibration.dig_P2 = ((int16_t)((readRegister(BME280_DIG_P2_MSB_REG) << 8) + readRegister(BME280_DIG_P2_LSB_REG)));
-	calibration.dig_P3 = ((int16_t)((readRegister(BME280_DIG_P3_MSB_REG) << 8) + readRegister(BME280_DIG_P3_LSB_REG)));
-	calibration.dig_P4 = ((int16_t)((readRegister(BME280_DIG_P4_MSB_REG) << 8) + readRegister(BME280_DIG_P4_LSB_REG)));
-	calibration.dig_P5 = ((int16_t)((readRegister(BME280_DIG_P5_MSB_REG) << 8) + readRegister(BME280_DIG_P5_LSB_REG)));
-	calibration.dig_P6 = ((int16_t)((readRegister(BME280_DIG_P6_MSB_REG) << 8) + readRegister(BME280_DIG_P6_LSB_REG)));
-	calibration.dig_P7 = ((int16_t)((readRegister(BME280_DIG_P7_MSB_REG) << 8) + readRegister(BME280_DIG_P7_LSB_REG)));
-	calibration.dig_P8 = ((int16_t)((readRegister(BME280_DIG_P8_MSB_REG) << 8) + readRegister(BME280_DIG_P8_LSB_REG)));
-	calibration.dig_P9 = ((int16_t)((readRegister(BME280_DIG_P9_MSB_REG) << 8) + readRegister(BME280_DIG_P9_LSB_REG)));
+	calibrationData.dig_P1 = ((uint16_t)((readRegister(BME280_DIG_P1_MSB_REG) << 8) + readRegister(BME280_DIG_P1_LSB_REG)));
+	calibrationData.dig_P2 = ((int16_t)((readRegister(BME280_DIG_P2_MSB_REG) << 8) + readRegister(BME280_DIG_P2_LSB_REG)));
+	calibrationData.dig_P3 = ((int16_t)((readRegister(BME280_DIG_P3_MSB_REG) << 8) + readRegister(BME280_DIG_P3_LSB_REG)));
+	calibrationData.dig_P4 = ((int16_t)((readRegister(BME280_DIG_P4_MSB_REG) << 8) + readRegister(BME280_DIG_P4_LSB_REG)));
+	calibrationData.dig_P5 = ((int16_t)((readRegister(BME280_DIG_P5_MSB_REG) << 8) + readRegister(BME280_DIG_P5_LSB_REG)));
+	calibrationData.dig_P6 = ((int16_t)((readRegister(BME280_DIG_P6_MSB_REG) << 8) + readRegister(BME280_DIG_P6_LSB_REG)));
+	calibrationData.dig_P7 = ((int16_t)((readRegister(BME280_DIG_P7_MSB_REG) << 8) + readRegister(BME280_DIG_P7_LSB_REG)));
+	calibrationData.dig_P8 = ((int16_t)((readRegister(BME280_DIG_P8_MSB_REG) << 8) + readRegister(BME280_DIG_P8_LSB_REG)));
+	calibrationData.dig_P9 = ((int16_t)((readRegister(BME280_DIG_P9_MSB_REG) << 8) + readRegister(BME280_DIG_P9_LSB_REG)));
 
-	calibration.dig_H1 = ((uint8_t)(readRegister(BME280_DIG_H1_REG)));
-	calibration.dig_H2 = ((int16_t)((readRegister(BME280_DIG_H2_MSB_REG) << 8) + readRegister(BME280_DIG_H2_LSB_REG)));
-	calibration.dig_H3 = ((uint8_t)(readRegister(BME280_DIG_H3_REG)));
-	calibration.dig_H4 = ((int16_t)((readRegister(BME280_DIG_H4_MSB_REG) << 4) + (readRegister(BME280_DIG_H4_LSB_REG) & 0x0F)));
-	calibration.dig_H5 = ((int16_t)((readRegister(BME280_DIG_H5_MSB_REG) << 4) + ((readRegister(BME280_DIG_H4_LSB_REG) >> 4) & 0x0F)));
-	calibration.dig_H6 = ((uint8_t)readRegister(BME280_DIG_H6_REG));
-
-	//Set the oversampling control words.
-	//config will only be writeable in sleep mode, so first insure that.
-	writeRegister(BME280_CTRL_MEAS_REG, 0x00);
-	
-	//Set the config word
-	dataToWrite = (settings.tStandby << 0x5) & 0xE0;
-	dataToWrite |= (settings.filter << 0x02) & 0x1C;
-	writeRegister(BME280_CONFIG_REG, dataToWrite);
-	
-	//Set ctrl_hum first, then ctrl_meas to activate ctrl_hum
-	dataToWrite = settings.humidOverSample & 0x07; //all other bits can be ignored
-	writeRegister(BME280_CTRL_HUMIDITY_REG, dataToWrite);
-	
-	//set ctrl_meas
-	//First, set temp oversampling
-	dataToWrite = (settings.tempOverSample << 0x5) & 0xE0;
-	//Next, pressure oversampling
-	dataToWrite |= (settings.pressOverSample << 0x02) & 0x1C;
-	//Last, set mode
-	dataToWrite |= (settings.runMode) & 0x03;
-	//Load the byte
-	writeRegister(BME280_CTRL_MEAS_REG, dataToWrite);
+	calibrationData.dig_H1 = ((uint8_t)(readRegister(BME280_DIG_H1_REG)));
+	calibrationData.dig_H2 = ((int16_t)((readRegister(BME280_DIG_H2_MSB_REG) << 8) + readRegister(BME280_DIG_H2_LSB_REG)));
+	calibrationData.dig_H3 = ((uint8_t)(readRegister(BME280_DIG_H3_REG)));
+	calibrationData.dig_H4 = ((int16_t)((readRegister(BME280_DIG_H4_MSB_REG) << 4) + (readRegister(BME280_DIG_H4_LSB_REG) & 0x0F)));
+	calibrationData.dig_H5 = ((int16_t)((readRegister(BME280_DIG_H5_MSB_REG) << 4) + ((readRegister(BME280_DIG_H4_LSB_REG) >> 4) & 0x0F)));
+	calibrationData.dig_H6 = ((uint8_t)readRegister(BME280_DIG_H6_REG));
     
-    ForceReading();
-	
 	return readRegister(0xD0);
 }
 
-void BME280::ForceReading(void)
+BME280Reading BME280::TakeReading(void)
 {
-    //set ctrl_meas
-    //First, set temp oversampling
-    uint8_t dataToWrite = (settings.tempOverSample << 0x5) & 0xE0;
-    //Next, pressure oversampling
-    dataToWrite |= (settings.pressOverSample << 0x02) & 0x1C;
-    //Last, set mode
-    dataToWrite |= 0x01; //(settings.runMode) & 0x03;
-    //Load the byte
-    writeRegister(BME280_CTRL_MEAS_REG, dataToWrite);
-    state = MEASURING;
-}
+    //must be in sleep mode to write to registers
+    writeRegister(BME280_CTRL_MEAS_REG, 0x00);
 
-//Strictly resets.  Run .begin() afterwards
-void BME280::reset( void )
-{
-	writeRegister(BME280_RST_REG, 0xB6);
-}
-
-uint8_t BME280::ReadStatus(void)
-{
-    return readRegister(BME280_STAT_REG);
-}
-
-uint8_t BME280::IsMeasuring(void)
-{
-    return ReadStatus() & 0x08;
-}
-
-bool BME280::CheckForNewDatum(void)
-{
-    bool retVal = false;
-    if(state == MEASURING && !IsMeasuring()) //current measurement is complete
-    {
-        workingDatum.timestamp = millis();
-        state = STANDBY;
-        retVal = true;
-    }
+    //Take one reading, no oversampling, no IIR
+    //write to the config register
+    writeRegister(BME280_CONFIG_REG, 0x00);
     
-    return retVal;
-}
+    //write humidity settings before temp and pressure
+    writeRegister(BME280_CTRL_HUMIDITY_REG, 0x01);
 
-/*
- * Function to read all the data registers at once. Not only is this faster than the poorly
- * thought out method in the original code, but it's also recommended by the data sheet,
- * since reading a byte at a time leads to inconsistent readings.
- */
-
-bool BME280::ReadDatum(void)
-{
-    Wire.beginTransmission(settings.I2CAddress);
+    //then write temperature, pressure and mode settings -- one shot
+    writeRegister(BME280_CTRL_MEAS_REG, 0x26);
+    
+    //wait until the measurement is done
+    while(readRegister(BME280_STAT_REG) & 0x08) {} //wait until measurement is done
+    
+    //measurement is done; retrieve the data
+    Wire.beginTransmission(bme280Addr);
     Wire.write(0xf7);
     Wire.endTransmission();
     
     // request bytes from sensor
     uint8_t data[8];
-    Wire.requestFrom(settings.I2CAddress, 8);
+    Wire.requestFrom(bme280Addr, 8);
     uint8_t i = 0;
     while (Wire.available() && (i < 8))  // slave may send more than requested
     {
         data[i++] = Wire.read(); // receive a byte as character
     }
 
+    //OK, now we have a measurement, so calculate the readings
+    BME280Reading reading;
+    
     //do temperature first to update t_fine, a carryover adjustment parameter
     int32_t t_fine = 0;
-
+    
     int32_t adc_T = ((uint32_t)data[3] << 12) | ((uint32_t)data[4] << 4) | ((data[5] >> 4) & 0x0F);
-    workingDatum.temperature = CalcTemperature(adc_T, t_fine);
+    reading.temperature = CalcTemperature(adc_T, t_fine);
     
     int32_t adc_P = ((uint32_t)data[0] << 12) | ((uint32_t)data[1] << 4) | ((data[2] >> 4) & 0x0F);
-    workingDatum.pressure = CalcPressure(adc_P, t_fine);
-    workingDatum.altitude = (-45846.2)*(pow((workingDatum.pressure/101325.0), 0.190263) - 1.0);
+    reading.pressure = CalcPressure(adc_P, t_fine);
+    reading.altitude = CalcAltitude(reading.pressure);
 
     int32_t adc_H = ((uint32_t)data[6] << 8) | ((uint32_t)data[7]);
-    workingDatum.humidity = CalcHumidity(adc_H, t_fine);
-    
-    return 1;
+    reading.humidity = CalcHumidity(adc_H, t_fine);
+
+    return reading;
 }
 
 float BME280::CalcPressure( int32_t adc_P, int32_t t_fine )
@@ -183,20 +165,20 @@ float BME280::CalcPressure( int32_t adc_P, int32_t t_fine )
 	
 	int64_t var1, var2, p_acc;
 	var1 = ((int64_t)t_fine) - 128000;
-	var2 = var1 * var1 * (int64_t)calibration.dig_P6;
-	var2 = var2 + ((var1 * (int64_t)calibration.dig_P5)<<17);
-	var2 = var2 + (((int64_t)calibration.dig_P4)<<35);
-	var1 = ((var1 * var1 * (int64_t)calibration.dig_P3)>>8) + ((var1 * (int64_t)calibration.dig_P2)<<12);
-	var1 = (((((int64_t)1)<<47)+var1))*((int64_t)calibration.dig_P1)>>33;
+	var2 = var1 * var1 * (int64_t)calibrationData.dig_P6;
+	var2 = var2 + ((var1 * (int64_t)calibrationData.dig_P5)<<17);
+	var2 = var2 + (((int64_t)calibrationData.dig_P4)<<35);
+	var1 = ((var1 * var1 * (int64_t)calibrationData.dig_P3)>>8) + ((var1 * (int64_t)calibrationData.dig_P2)<<12);
+	var1 = (((((int64_t)1)<<47)+var1))*((int64_t)calibrationData.dig_P1)>>33;
 	if (var1 == 0)
 	{
 		return 0; // avoid exception caused by division by zero
 	}
 	p_acc = (int32_t)1048576 - adc_P;
 	p_acc = (((p_acc<<31) - var2)*3125)/var1;
-	var1 = (((int64_t)calibration.dig_P9) * (p_acc>>13) * (p_acc>>13)) >> 25;
-	var2 = (((int64_t)calibration.dig_P8) * p_acc) >> 19;
-	p_acc = ((p_acc + var1 + var2) >> 8) + (((int64_t)calibration.dig_P7)<<4);
+	var1 = (((int64_t)calibrationData.dig_P9) * (p_acc>>13) * (p_acc>>13)) >> 25;
+	var2 = (((int64_t)calibrationData.dig_P8) * p_acc) >> 19;
+	p_acc = ((p_acc + var1 + var2) >> 8) + (((int64_t)calibrationData.dig_P7)<<4);
 	
 	return p_acc / 256.0;
 }
@@ -213,10 +195,10 @@ float BME280::CalcHumidity( int32_t adc_H, int32_t t_fine )
     
 	int32_t var1;
 	var1 = (t_fine - ((int32_t)76800));
-	var1 = (((((adc_H << 14) - (((int32_t)calibration.dig_H4) << 20) - (((int32_t)calibration.dig_H5) * var1)) +
-	((int32_t)16384)) >> 15) * (((((((var1 * ((int32_t)calibration.dig_H6)) >> 10) * (((var1 * ((int32_t)calibration.dig_H3)) >> 11) + ((int32_t)32768))) >> 10) + ((int32_t)2097152)) *
-	((int32_t)calibration.dig_H2) + 8192) >> 14));
-	var1 = (var1 - (((((var1 >> 15) * (var1 >> 15)) >> 7) * ((int32_t)calibration.dig_H1)) >> 4));
+	var1 = (((((adc_H << 14) - (((int32_t)calibrationData.dig_H4) << 20) - (((int32_t)calibrationData.dig_H5) * var1)) +
+	((int32_t)16384)) >> 15) * (((((((var1 * ((int32_t)calibrationData.dig_H6)) >> 10) * (((var1 * ((int32_t)calibrationData.dig_H3)) >> 11) + ((int32_t)32768))) >> 10) + ((int32_t)2097152)) *
+	((int32_t)calibrationData.dig_H2) + 8192) >> 14));
+	var1 = (var1 - (((((var1 >> 15) * (var1 >> 15)) >> 7) * ((int32_t)calibrationData.dig_H1)) >> 4));
 	var1 = (var1 < 0 ? 0 : var1);
 	var1 = (var1 > 419430400 ? 419430400 : var1);
 
@@ -230,9 +212,9 @@ float BME280::CalcTemperature( int32_t adc_T, int32_t& t_fine ) //C
 
 	int64_t var1, var2;
 
-	var1 = ((((adc_T>>3) - ((int32_t)calibration.dig_T1<<1))) * ((int32_t)calibration.dig_T2)) >> 11;
-	var2 = (((((adc_T>>4) - ((int32_t)calibration.dig_T1)) * ((adc_T>>4) - ((int32_t)calibration.dig_T1))) >> 12) *
-	((int32_t)calibration.dig_T3)) >> 14;
+	var1 = ((((adc_T>>3) - ((int32_t)calibrationData.dig_T1<<1))) * ((int32_t)calibrationData.dig_T2)) >> 11;
+	var2 = (((((adc_T>>4) - ((int32_t)calibrationData.dig_T1)) * ((adc_T>>4) - ((int32_t)calibrationData.dig_T1))) >> 12) *
+	((int32_t)calibrationData.dig_T3)) >> 14;
 	t_fine = var1 + var2;
 	float output = (t_fine * 5 + 128) >> 8;
 
@@ -247,15 +229,15 @@ float BME280::CalcTemperature( int32_t adc_T, int32_t& t_fine ) //C
 //
 //****************************************************************************//
 
-uint8_t BME280::readRegister(const uint8_t& offset)
+uint8_t BME280::readRegister(const uint8_t& regAddr)
 {
     uint8_t result = 0;
     
-    Wire.beginTransmission(settings.I2CAddress);
-    Wire.write(offset);
+    Wire.beginTransmission(bme280Addr);
+    Wire.write(regAddr);
     Wire.endTransmission();
 
-    Wire.requestFrom(settings.I2CAddress, 1);
+    Wire.requestFrom(bme280Addr, 1);
     while ( Wire.available() ) // slave may send less than requested
     {
         result = Wire.read(); // receive a byte as a proper uint8_t
@@ -264,11 +246,11 @@ uint8_t BME280::readRegister(const uint8_t& offset)
 	return result;
 }
 
-void BME280::writeRegister(const uint8_t& offset, const uint8_t& dataToWrite)
+void BME280::writeRegister(const uint8_t& regAddr, const uint8_t& dataToWrite)
 {
     //Write the byte
-    Wire.beginTransmission(settings.I2CAddress);
-    Wire.write(offset);
+    Wire.beginTransmission(bme280Addr);
+    Wire.write(regAddr);
     Wire.write(dataToWrite);
     Wire.endTransmission();
 }
--- BME280.h
+++ BME280.h
@@ -24,87 +24,10 @@ Distributed as-is; no warranty is given.
 #define __BME280_H__
 
 #include "stdint.h"
-#include <altimeter.h>
-
-#define I2C_MODE 0
-#define SPI_MODE 1
-
-//Register names:
-#define BME280_DIG_T1_LSB_REG			0x88
-#define BME280_DIG_T1_MSB_REG			0x89
-#define BME280_DIG_T2_LSB_REG			0x8A
-#define BME280_DIG_T2_MSB_REG			0x8B
-#define BME280_DIG_T3_LSB_REG			0x8C
-#define BME280_DIG_T3_MSB_REG			0x8D
-#define BME280_DIG_P1_LSB_REG			0x8E
-#define BME280_DIG_P1_MSB_REG			0x8F
-#define BME280_DIG_P2_LSB_REG			0x90
-#define BME280_DIG_P2_MSB_REG			0x91
-#define BME280_DIG_P3_LSB_REG			0x92
-#define BME280_DIG_P3_MSB_REG			0x93
-#define BME280_DIG_P4_LSB_REG			0x94
-#define BME280_DIG_P4_MSB_REG			0x95
-#define BME280_DIG_P5_LSB_REG			0x96
-#define BME280_DIG_P5_MSB_REG			0x97
-#define BME280_DIG_P6_LSB_REG			0x98
-#define BME280_DIG_P6_MSB_REG			0x99
-#define BME280_DIG_P7_LSB_REG			0x9A
-#define BME280_DIG_P7_MSB_REG			0x9B
-#define BME280_DIG_P8_LSB_REG			0x9C
-#define BME280_DIG_P8_MSB_REG			0x9D
-#define BME280_DIG_P9_LSB_REG			0x9E
-#define BME280_DIG_P9_MSB_REG			0x9F
-#define BME280_DIG_H1_REG				0xA1
-#define BME280_CHIP_ID_REG				0xD0 //Chip ID
-#define BME280_RST_REG					0xE0 //Softreset Reg
-#define BME280_DIG_H2_LSB_REG			0xE1
-#define BME280_DIG_H2_MSB_REG			0xE2
-#define BME280_DIG_H3_REG				0xE3
-#define BME280_DIG_H4_MSB_REG			0xE4
-#define BME280_DIG_H4_LSB_REG			0xE5
-#define BME280_DIG_H5_MSB_REG			0xE6
-#define BME280_DIG_H6_REG				0xE7
-#define BME280_CTRL_HUMIDITY_REG		0xF2 //Ctrl Humidity Reg
-#define BME280_STAT_REG					0xF3 //Status Reg
-#define BME280_CTRL_MEAS_REG			0xF4 //Ctrl Measure Reg
-#define BME280_CONFIG_REG				0xF5 //Configuration Reg
-#define BME280_PRESSURE_MSB_REG			0xF7 //Pressure MSB
-#define BME280_PRESSURE_LSB_REG			0xF8 //Pressure LSB
-#define BME280_PRESSURE_XLSB_REG		0xF9 //Pressure XLSB
-#define BME280_TEMPERATURE_MSB_REG		0xFA //Temperature MSB
-#define BME280_TEMPERATURE_LSB_REG		0xFB //Temperature LSB
-#define BME280_TEMPERATURE_XLSB_REG		0xFC //Temperature XLSB
-#define BME280_HUMIDITY_MSB_REG			0xFD //Humidity MSB
-#define BME280_HUMIDITY_LSB_REG			0xFE //Humidity LSB
-
-
-//Class BME280Settings.  This object is used to hold settings data.  The application
-//uses this class's data directly.  The settings are adopted and sent to the sensor
-//at special times, such as .begin.  Some are used for doing math.
-//
-//This is a kind of bloated way to do this.  The trade-off is that the user doesn't
-//need to deal with #defines or enums with bizarre names.
-//
-//A power user would strip out SensorSettings entirely, and send specific read and
-//write command directly to the IC. (ST #defines below)
-//
-struct BME280Settings
-{
-  public:
-    uint8_t I2CAddress = 0x77;
-    uint8_t chipSelectPin = -1;
-	
-	uint8_t runMode = 0;
-	uint8_t tStandby = 1;
-	uint8_t filter = 0;
-	uint8_t tempOverSample = 5;
-	uint8_t pressOverSample = 5;
-	uint8_t humidOverSample = 5;
-};
 
 //Used to hold the calibration constants.  These are used
 //by the driver as measurements are being taking
-struct SensorCalibration
+struct CalibrationData
 {
   public:
 	uint16_t dig_T1;
@@ -127,18 +50,22 @@ struct SensorCalibration
 	int16_t dig_H4;
 	int16_t dig_H5;
 	uint8_t dig_H6;
-	
 };
 
-//This is the man operational class of the driver.
-class BME280 : public Altimeter
+struct BME280Reading
+{
+    float temperature = 0;
+    float humidity = 0;
+    float pressure = 0;
+    float altitude = 0;
+};
+
+//This is the main operational class of the driver.
+class BME280
 {
 protected:
-    enum STATE {STANDBY, MEASURING};
-    STATE state = STANDBY;
-    
-    BME280Settings settings;
-    SensorCalibration calibration;
+    const uint8_t bme280Addr;
+    CalibrationData calibrationData;
 
     //helper functions for converting raw readings to useful values
     float CalcTemperature( int32_t, int32_t& );
@@ -150,20 +77,10 @@ protected:
     void writeRegister(const uint8_t&, const uint8_t& );
 
 public:
-    BME280( void ) {}
+    BME280( uint8_t addr ) : bme280Addr(addr) {}
 	
-    uint8_t Init( const BME280Settings& );
-
-	//Software reset routine
-	void reset( void );
-    void ForceReading(void);
-
-    bool CheckForNewDatum(void);
-    uint8_t IsMeasuring(void);
-    uint8_t ReadStatus(void);
-
-    bool ReadDatum(void);
+    uint8_t Init(void);
+    BME280Reading TakeReading(void);
 };
-
 
 #endif  // End of __BME280_H__ definition check
